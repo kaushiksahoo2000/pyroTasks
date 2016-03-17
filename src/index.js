56a4363a71b9c03a219cabc4708a47f0a4b57ca9
Joseph Martin <jpranaymartin@gmail.com>
2016-03-17 14:41:00 -0700

    [bugfix] cleanup unnecessary comments and add babel stage-1 preset

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-/* ... */
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-/* broswerHistory is used to assess context */
 import {Router, browserHistory} from 'react-router';
 import Routes from './routes';
 
--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,15 @@
 import React from 'react'
 import {Route, IndexRoute} from 'react-router'
 
-/**
- * Import components here
- */
 import App from './components/app'
 import AuthView from './components/auth_view'
 import LoginForm from './components/login_form'
 import SignupForm from './components/signup_form'
 
-
-
 export default (
   <Route path='/' component={App}>
     <IndexRoute component={AuthView}/>
     <Route path='signup' component={SignupForm}/>
-    <Route path='login' component={LoginForm}/>
+    {/*<Route path='login' component={LoginForm}/>*/}
   </Route>
 )
