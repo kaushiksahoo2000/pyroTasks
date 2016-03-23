90eabf791089353259264c180e36f1e74863ae18
Joseph Martin <jpranaymartin@gmail.com>
2016-03-23 16:39:04 -0700

    Add actions for signup and login modules

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,14 @@ import {Route, IndexRoute} from 'react-router'
 
 import App from './components/app'
 import AuthView from './components/auth_view'
-import LoginForm from './components/login_form'
 import SignupForm from './components/signup_form'
+import LoginForm from './components/login_form'
 
 export default (
   <Route path='/' component={App}>
     <IndexRoute component={AuthView}/>
     <Route path='signup' component={SignupForm}/>
-    {/*<Route path='login' component={LoginForm}/>*/}
+    <Route path='login' component={LoginForm}/>
+    <Route path='dashboard' component={Dashboard}/>
   </Route>
 )
