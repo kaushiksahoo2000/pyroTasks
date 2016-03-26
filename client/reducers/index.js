2ec3e784b4caa5402f334991a9418d42bbd95547
Joseph Martin <jpranaymartin@gmail.com>
2016-03-25 23:06:25 -0700

    [feature] Changed folder structure to allow for Express server

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,10 +2,11 @@ import { combineReducers } from 'redux';
 
 import groupsReducer from './reducer_groups'
 
-
+import {reducer as formReducer} from 'redux-form'
 
 const rootReducer = combineReducers({
+  form: formReducer,
   groups: groupsReducer
 });
 
-export default rootReducer;
+export default rootReducer
